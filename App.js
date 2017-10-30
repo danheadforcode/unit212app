55fc1bd7cf8fd232db7828cce66013516a95bed5
Nick Lewis <nickeblewis@gmail.com>
2017-10-30 11:11:06 +0000

    Adding comments

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
+/**
+ * App.js is the starting point for this app
+ */
+
+ /**
+  * The following import section is responsible for importing libraries, components and other assets that this file requires
+  */
 import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font } from 'expo';
@@ -5,14 +12,20 @@ import { Ionicons } from '@expo/vector-icons';
 import RootNavigation from './navigation/RootNavigation';
 
 export default class App extends React.Component {
+
+  // This is a simple state object which we use to determine whether assets are still being loaded
   state = {
     assetsAreLoaded: false,
   };
 
+  // Function that handles the initial creation of this component, it calls another function, see below
   componentWillMount() {
     this._loadAssetsAsync();
   }
 
+  // App.js is in itself a component that loads others to generate the app, every React component must have a render function
+  // the render function handles the output of the UI
+  // Initially this will show a splash screen for HomeBrew and then will seemlessly switch to the actual application when ready
   render() {
     if (!this.state.assetsAreLoaded && !this.props.skipLoadingScreen) {
       return <AppLoading />;
@@ -28,6 +41,7 @@ export default class App extends React.Component {
     }
   }
 
+  // When the component is mounting, in other words being created, we call this function which loads icons, fonts and splash screen
   async _loadAssetsAsync() {
     try {
       await Promise.all([
@@ -57,6 +71,8 @@ export default class App extends React.Component {
   }
 }
 
+// Every React component has it's own style sheet which defines the look of the app
+// Note that this is expressed as an Object
 const styles = StyleSheet.create({
   container: {
     flex: 1,
